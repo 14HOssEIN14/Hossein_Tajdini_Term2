b18f7490b8b6969d70f4df12ba44c64bcb26454f
hossein <hosseintajeddini000@gmail.com>
2025-04-26 14:10:47 +0330

    EX2

---
+++ EX2/Intervals/IntervalIntegration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervals
+{
+    public class IntervalIntegration
+    {
+
+        public List<Interval> Intervals = new();
+        public List<Interval> Integration(List<Interval> interval)
+        {
+
+            var sortedIntervals = interval.OrderBy(interval => interval.Start).ToList();
+
+            for (int i = 0; i < sortedIntervals.Count; i++)
+            {
+                if (i + 1 < sortedIntervals.Count)
+                {
+
+                    if (sortedIntervals[i].Members.Union(sortedIntervals[i + 1].Members).SequenceEqual(new Interval { Start = sortedIntervals[i].Start, End = sortedIntervals[i + 1].End }.Members))
+                    {
+                        sortedIntervals[i] = new Interval { Start = sortedIntervals[i].Start, End = sortedIntervals[i + 1].End };
+                        sortedIntervals.RemoveAt(i + 1);
+                    }
+
+                }
+            }
+            return sortedIntervals;
+        }
+
+    }
+    public record Interval
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        private List<int> members = new List<int>();
+        public List<int> Members
+        {
+            get
+            {
+                members = new List<int>();
+                for (int i = Start; i <= End; i++)
+                {
+                    members.Add(i);
+                }
+                return members;
+            }
+            set { }
+        }
+    }
+}
--- EX2/Intervals/Program.cs
+++ EX2/Intervals/Program.cs
@@ -1,2 +1,38 @@
-﻿
-Console.WriteLine(    "test");
+﻿using Intervals;
+#region test1
+//List<Interval> INTERVALS = new List<Interval> {
+//    new Interval{Start=1,End=3},
+//    new Interval{Start=2,End=6},
+//    new Interval{Start=8,End=10},
+//    new Interval{Start=15,End=18},
+
+//};
+//IntervalIntegration intervalIntegration = new IntervalIntegration();
+
+//var result = intervalIntegration.Integration(INTERVALS);
+//foreach (var x in result)
+//{
+//    foreach (var y in x.Members)
+//    {
+//        Console.Write(y.ToString() + ",");
+//    }
+//    Console.WriteLine("\n#########################");
+//}
+#endregion
+#region test2
+List<Interval> INTERVALS = new List<Interval> {
+    new Interval{Start=1,End=4},
+    new Interval{Start=4,End=5}
+};
+IntervalIntegration intervalIntegration = new IntervalIntegration();
+
+var result = intervalIntegration.Integration(INTERVALS);
+foreach (var x in result)
+{
+    foreach (var y in x.Members)
+    {
+        Console.Write(y.ToString() + ",");
+    }
+    Console.WriteLine("\n#########################");
+}
+#endregion
